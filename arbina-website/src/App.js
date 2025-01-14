4ce783359a67273dd415aa0a48464d5e5df0173f
Omer Mohiuddin <mdomermohi@gmail.com>
2025-01-13 19:52:54 -0500

    Fix submodule issue

---
+++ arbina-website/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppTheme from './shared-theme/AppTheme';
+import MarketingPage from './marketing-page/MarketingPage';
+
+function App() {
+  return (
+    <AppTheme>
+      <MarketingPage />
+    </AppTheme>
+  );
+}
+
+export default App;
